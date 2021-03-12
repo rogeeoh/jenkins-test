2580ccfe756c3ff0d76f30088ae8f7b8b6792fc3
ohrogee <rogeeoh@gmail.com>
2021-03-12 16:47:53 +0900

    ADD: version 9 && added test

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,6 @@ class App {
 const app = new App().application;
 
 app.get("/",(req : express.Request , res : express.Response) =>{
-    res.send("v8");
+    res.send("v9");
 })
 app.listen(4050,()=>console.log("start"));
---
+++ test/test1.spec.js
@@ -0,0 +1,17 @@
+const sayHello = () => {
+    return 'hello';
+};
+
+describe('Test!!', () => {
+    it('say Hello should return hello', (done) => {
+        if (sayHello() === 'hello') {
+            done();
+        }
+    });
+
+    it('say Hello should return hello', (done) => {
+        if (sayHello() === 'bye') {
+            done();
+        }
+    });
+});
