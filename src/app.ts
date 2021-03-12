7f0611dcebfe8e9d0f77bc5d66131867252d96d4
ohrogee <rogeeoh@gmail.com>
2021-03-12 14:25:45 +0900

    ADD: version8

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,6 @@ class App {
 const app = new App().application;
 
 app.get("/",(req : express.Request , res : express.Response) =>{
-    res.send("v7");
+    res.send("v8");
 })
 app.listen(4050,()=>console.log("start"));
