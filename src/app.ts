add0d3428a48fbc4fa6159e46a6a62c77d61500b
ohrogee <rogeeoh@gmail.com>
2021-04-06 12:59:30 +0900

    ADD: version 2

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ class App {
 const app = new App().application;
 
 app.get("/",(req : express.Request , res : express.Response) =>{
-    res.send("v1");
+    res.send("v2");
 })
 app.listen(PORT,()=> {
     console.log(`server starts at ${PORT}`);
