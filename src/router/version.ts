0094778e7936da4e5f7474f7782afd4b9098d915
ohrogee <rogeeoh@gmail.com>
2021-04-07 13:33:35 +0900

    ADD: version is 5

--- src/router/version.ts
+++ src/router/version.ts
@@ -1,10 +1,10 @@
 import express, {Request, Response, Router} from "express";
 
 const router:Router = express.Router();
-const version:Number = 4;
+const version:Number = 5;
 
 router.get('/', (req: Request, res: Response) => {
-    res.send(`<h1>This is version ${version}</h1>`);
+    res.send(`<h1>This is version ${version}</h1></br>`);
 });
 
 export default router;
