1c2f57ac75a0dd250274c746be2929156e112c99
ohrogee <rogeeoh@gmail.com>
2021-04-07 13:14:18 +0900

    ADD: hostname is added

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,11 @@ import express, {Request, Response, Router} from "express";
 const router:Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Hello Express with Typescript!</h1>');
+    res.send(`<h1>Hello Express with Typescript!</h1>`);
+});
+
+router.get('/hostname', (req: Request, res: Response) => {
+    res.send(`<h1>hostname: ${process.env.HOSTNAME}</h1>`);
 });
 
 export default router;
